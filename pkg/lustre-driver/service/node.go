108e080b8783f9f825a994a985ede23315565a39
Dean Roehrich <dean.roehrich@hpe.com>
2025-09-25 09:17:26 -0500

    more info
    Signed-off-by: Dean Roehrich <dean.roehrich@hpe.com>

--- pkg/lustre-driver/service/node.go
+++ pkg/lustre-driver/service/node.go
@@ -54,13 +54,6 @@ func (s *service) NodePublishVolume(
 	req *csi.NodePublishVolumeRequest) (
 	*csi.NodePublishVolumeResponse, error) {
 
-	panic("XXX DEANDEAN")
-}
-
-func (s *service) xxNodePublishVolume(
-	ctx context.Context,
-	req *csi.NodePublishVolumeRequest) (
-	*csi.NodePublishVolumeResponse, error) {
 	// 1. Validate request
 	if req.GetVolumeId() == "" {
 		return nil, status.Errorf(codes.InvalidArgument, "NodePublishVolume - VolumeID is required")
